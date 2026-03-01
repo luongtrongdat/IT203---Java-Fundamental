58c4e6a0b71c2676aec3781a67722cd76e84a295
PinkD <luongdat114zu@gmail.com>
2026-03-01 22:00:18 +0700

    Add files via upload

---
+++ src/com/session14/PatientTreatment.java
@@ -0,0 +1,18 @@
+package com.session14;
+
+public class PatientTreatment {
+    String name;
+    int age;
+    String major;
+
+    public PatientTreatment(String name, int age, String major) {
+        this.name = name;
+        this.age = age;
+        this.major = major;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + age + " tuổi - " + major;
+    }
+}
